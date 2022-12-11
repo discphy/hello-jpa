74b243a7e985d71564c48bf632bcfee93bfc8598
discphy <dinophy@nate.com>
2022-12-12 08:34:03 +0900

    feat(add): JPA jpql

--- src/main/java/com/hello/jpa/JpaMain.java
+++ src/main/java/com/hello/jpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -16,36 +17,11 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Address address = new Address("homeCity", "street", "zipcode");
+            List<Member> resultList = em.createQuery("select m from Member m where m.username like '%kim%'", Member.class).getResultList();
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(address);
-
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "zipcode"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "zipcode"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("=========================");
-            Member findMember = em.find(Member.class, member.getId());
-
-            //findMember.getHomeAddress().setCity("newCity");
-            Address homeAddress = findMember.getHomeAddress();
-            findMember.setHomeAddress(new Address("newCity", homeAddress.getStreet(), homeAddress.getZipcode()));
-
-            findMember.getFavoriteFoods().remove("치킨");
-            findMember.getFavoriteFoods().add("한식");
-
-            findMember.getAddressHistory().remove(new AddressEntity("old1", "street", "zipcode"));
-            findMember.getAddressHistory().add(new AddressEntity("newCity1", "street", "zipcode"));
+            for (Member member : resultList) {
+                System.out.println("member = " + member);
+            }
 
             tx.commit();
         } catch (Exception e) {
