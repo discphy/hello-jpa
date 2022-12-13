869eb42608be4fc04258c1a8f47ef747c0b5143e
discphy <dinophy@nate.com>
2022-12-13 09:11:40 +0900

    feat(add): JPA Native sql

--- src/main/java/com/hello/jpa/JpaMain.java
+++ src/main/java/com/hello/jpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -16,7 +17,15 @@ public class JpaMain {
         tx.begin();
 
         try {
-            em.createNativeQuery("select MEMBER_ID, city, USERNAME form MEMBER").getResultList();
+            Member member = new Member();
+            member.setUsername("member1");
+            em.persist(member);
+
+            List<Member> resultList = em.createNativeQuery("select MEMBER_ID, city, USERNAME form MEMBER", Member.class).getResultList();
+
+            for (Member member1 : resultList) {
+                System.out.println("member1 = " + member1);
+            }
 
             tx.commit();
         } catch (Exception e) {
